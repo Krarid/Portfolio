f218c72c45ce50af0b8d8a2710ed88a02f15413e
Javier Meléndez <jmz.krarid@outlook.com>
2023-08-29 14:41:52 -0600

    Add default screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
