57a0a5db990d546f28886502c53cb1a241461b58
Javier Meléndez <jmz.krarid@outlook.com>
2023-09-03 03:21:11 -0600

    Add Zoom In and Zoom Out for certifications

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById('menu-btn')
+/* const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 
 // Hamburger button listener
@@ -8,4 +8,32 @@ function navToggle() {
     btn.classList.toggle('open')
     menu.classList.toggle('flex')
     menu.classList.toggle('hidden')
-}
+} */
+
+$( function() {
+    // Click on hamburger icon and then open mobile header
+    $('#menu-btn').on('click', (e) => {
+        $(e.target).parent().toggleClass('open')
+        $menu = $('#menu').toggleClass(['flex', 'hidden'])
+    })
+
+    // Click on any certificate and then zoom in
+    $('.certificate').on('click', (e) => {
+        const certificate = $(e.target).attr('src')
+        $('#full-certificate').children('img').attr('src', certificate)
+        $('#full-certificate').addClass('flex').removeClass('hidden')
+    })
+
+    // Click on full certificate and then zoom out
+    $('#full-certificate').on('click', () => {
+        $('#full-certificate').addClass('hidden').removeClass('flex')
+    })
+
+    // Get rid of container when inner width is less than 768px
+    $(window).on('resize', () => {
+        if( window.innerWidth < 768 )
+            $('#hero').removeClass('container', 'mx-auto')
+        else
+            $('#hero').addClass('container', 'mx-auto')
+    })
+});
